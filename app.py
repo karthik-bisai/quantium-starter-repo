bc0020f99482631f76c922a523cdfd91eca84ba6
karthik bisai <karthikbisaidev@gmail.com>
2026-01-28 14:32:09 +0530

    Initial Dashboard

---
+++ app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from dash import Dash, dcc, html
+import plotly.express as px
+
+df = pd.read_csv("pink_morsel_sales.csv")
+
+
+df["Date"] = pd.to_datetime(df["Date"])
+
+
+df = df.sort_values("Date")
+
+fig = px.line(
+    df,
+    x="Date",
+    y="Sales",
+    title="Pink Morsel Sales Over Time",
+    labels={
+        "Date": "Date",
+        "Sales": "Total Sales"
+    }
+)
+
+app = Dash(__name__)
+
+app.layout = html.Div(
+    children=[
+        html.H1(
+            "Pink Morsel Sales Visualiser",
+            style={"textAlign": "center"}
+        ),
+        dcc.Graph(figure=fig)
+    ]
+)
+
+if __name__ == "__main__":
+    app.run(debug=True)
